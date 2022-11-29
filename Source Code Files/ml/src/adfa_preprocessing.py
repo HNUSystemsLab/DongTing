1ad8711bcc94fb072afc5ff73f5b92ed9a87e10a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-29 09:10:15 +0000

    Adding tarfile member sanitization to extractall()

--- Source Code Files/ml/src/adfa_preprocessing.py
+++ Source Code Files/ml/src/adfa_preprocessing.py
@@ -89,7 +89,29 @@ def unpack_data():
 
     if not Path("../data/PLAID").exists():
         with tarfile.open("../data/PLAID.tar.xz") as f:
-            f.extractall(".")
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, ".")
 
 
 if __name__ == "__main__":
